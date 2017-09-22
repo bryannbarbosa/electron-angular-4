41615d98cc681d3786fe4d48a22bc13553c27b3f
bryannbarbosa <bryannbarbosa@gmail.com>
2017-09-21 22:28:36 -0300

    building windows installer

--- build.js
+++ build.js
@@ -1,10 +1,10 @@
 const electronInstaller = require('electron-winstaller');
 
 resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: './app-builds/sms-database-filter-win32-x64',
+    appDirectory: './app-builds/sms_database_filter-win32-x64',
     outputDirectory: './release/installer',
     authors: 'Bryann Barbosa',
-    exe: 'sms-database-filter.exe'
+    exe: 'sms_database_filter.exe'
   });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
--- main.ts
+++ main.ts
@@ -3,6 +3,13 @@ import * as electron from 'electron';
 import * as path from 'path';
 import * as fs from 'fs';
 
+if(require('electron-squirrel-startup')) app.quit();
+
+if (handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  app.quit()
+}
+
 const Excel = require('exceljs');
 const ipcMain = electron.ipcMain;
 const dialog = electron.dialog;
@@ -18,52 +25,7 @@ if (serve) {
   });
 }
 
-const squirrelUrl = "http://localhost:3333";
-
-const startAutoUpdater = (squirrelUrl) => {
-  // The Squirrel application will watch the provided URL
-  electron.autoUpdater.setFeedURL(`${squirrelUrl}/win64/`);
-
-  // Display a success message on successful update
-  electron.autoUpdater.addListener("update-downloaded", (event, releaseNotes, releaseName) => {
-    electron.dialog.showMessageBox({"message": `The release ${releaseName} has been downloaded`});
-  });
-
-  // Display an error message on update error
-  electron.autoUpdater.addListener("error", (error) => {
-    electron.dialog.showMessageBox({"message": "Auto updater error: " + error});
-  });
-
-  // tell squirrel to check for updates
-  electron.autoUpdater.checkForUpdates();
-}
-
-const handleSquirrelEvent = () => {
-  if (process.argv.length === 1) {
-    return false;
-  }
-
-  const squirrelEvent = process.argv[1];
-  switch (squirrelEvent) {
-    case '--squirrel-install':
-    case '--squirrel-updated':
-    case '--squirrel-uninstall':
-      setTimeout(app.quit, 1000);
-      return true;
-
-    case '--squirrel-obsolete':
-      app.quit();
-      return true;
-  }
-}
-
-if (handleSquirrelEvent()) {
-  // squirrel event handled and app will exit in 1000ms, so don't do anything else
-}
-
 function createWindow() {
-
-  if (process.env.NODE_ENV !== "dev") startAutoUpdater(squirrelUrl);
 
   const electronScreen = screen;
   const size = electronScreen.getPrimaryDisplay().workAreaSize;
@@ -219,3 +181,65 @@ ipcMain.on('startProcess', (event, args) => {
       });
   });
 });
+
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function(command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+    } catch (error) {}
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit();
+      return true;
+  }
+};
